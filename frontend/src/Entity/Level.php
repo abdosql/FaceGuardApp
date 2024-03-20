776a3dbd59bdf023a13cd0a13986bbb609828561
abdosql <abdosql@users.noreply.github.com>
2024-03-20 04:06:35 +0100

    Adding the Logo, Entities: Course Level Teacher Semestre

---
+++ frontend/src/Entity/Level.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LevelRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LevelRepository::class)]
+class Level
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $level_name = null;
+
+    #[ORM\OneToMany(targetEntity: Course::class, mappedBy: 'levels')]
+    private Collection $courses;
+
+    public function __construct()
+    {
+        $this->courses = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLevelName(): ?string
+    {
+        return $this->level_name;
+    }
+
+    public function setLevelName(string $level_name): static
+    {
+        $this->level_name = $level_name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Course>
+     */
+    public function getCourses(): Collection
+    {
+        return $this->courses;
+    }
+
+    public function addCourse(Course $course): static
+    {
+        if (!$this->courses->contains($course)) {
+            $this->courses->add($course);
+            $course->setLevels($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCourse(Course $course): static
+    {
+        if ($this->courses->removeElement($course)) {
+            // set the owning side to null (unless already changed)
+            if ($course->getLevels() === $this) {
+                $course->setLevels(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ frontend/src/Entity/Teacher.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeacherRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeacherRepository::class)]
+class Teacher
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToMany(targetEntity: Course::class, mappedBy: 'teacher')]
+    private Collection $Courses;
+
+    public function __construct()
+    {
+        $this->Courses = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Course>
+     */
+    public function getCourses(): Collection
+    {
+        return $this->Courses;
+    }
+
+    public function addCourse(Course $course): static
+    {
+        if (!$this->Courses->contains($course)) {
+            $this->Courses->add($course);
+            $course->setTeacher($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCourse(Course $course): static
+    {
+        if ($this->Courses->removeElement($course)) {
+            // set the owning side to null (unless already changed)
+            if ($course->getTeacher() === $this) {
+                $course->setTeacher(null);
+            }
+        }
+
+        return $this;
+    }
+}
