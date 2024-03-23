4337190ece69a78073992d03b391e760fc70e028
abdosql <abdosql@users.noreply.github.com>
2024-03-23 23:36:55 +0100

    Fixing some authSystem bugs, and Making teachers Management page

--- frontend/src/Controller/SecurityController.php
+++ frontend/src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -11,9 +12,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, EntityManagerInterface $entityManager): Response
+    public function login(AuthenticationUtils $authenticationUtils, Security $security): Response
     {
-
+        if ($security->isGranted("IS_AUTHENTICATED_FULLY")){
+            return $this->redirectToRoute("app_admin");
+        }
         return $this->render('security/login.html.twig', [
             "error" => $authenticationUtils->getLastAuthenticationError(),
             "last_username" => $authenticationUtils->getLastUsername(),
