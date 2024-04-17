83e594b8903e04b860bba6ad29fb1cd7f4a6eb2d
abdosql <abdosql@users.noreply.github.com>
2024-04-17 03:27:26 +0200

    Making Time schedule settings form inputs and adding some settings services

--- frontend/src/Controller/SettingsController.php
+++ frontend/src/Controller/SettingsController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/settings')]
 class SettingsController extends AbstractController
 {
-    #[Route('/settings', name: 'app_settings_index')]
+    #[Route('/', name: 'app_settings_index')]
     public function index(): Response
     {
         return $this->render('settings/index.html.twig', [
             'controller_name' => 'SettingsController',
         ]);
+    }
+    #[Route('/schedule', name: 'app_schedule_settings_index', methods: ["POST"])]
+
+    public function timeScheduleSettingsPage(Request $request): Response
+    {
+
+        return $this->render('settings/scheduleSetting.html.twig');
     }
 }
